9f2e9a4325a5106fec0a6af6881cdda39ca7f20a
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-12-09 09:16:58 +0100

    ..

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,27 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ange ditt namn: ");
+        System.out.print("Ange ditt lösenord: ");
 
         String namn = scanner.nextLine();
+        String losenord = scanner.nextLine();
 
-        System.out.println( "ditt namn är: " + namn);
+        while (true) {
+            System.out.println("ditt namn är: " + namn);
+            System.out.println("ditt lösenord är: " + losenord);
 
-        if (namn == "noname"){
+            if (namn.equals("noname") && losenord.equals("nopass")) {
+                System.out.println("Welcome!");
+                break;
+            } else {
+                System.out.println("Wrong");
+            }
 
         }
+
+        for (int i = 0; i < 32; i++) {
+            System.out.println("hello world!");
+        }
+
     }
 }
