04d42826efe8726735ce0a79abe7929be5635f9e
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-12-09 08:48:32 +0100

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        if (6 >= 3) {
+            System.out.println("Hello world!");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ange ditt namn: ");
+
+        String namn = scanner.nextLine();
+
+        System.out.println( "ditt namn är: " + namn);
+
+        if (namn == "noname"){
+
+        }
     }
 }
